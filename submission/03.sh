1387a77f4dd399891e07910d7531358a727fefd0
72kb <lanniox@gmail.com>
2026-03-24 21:15:54 +0100

    First Commit

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,6 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+ADDRESS=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+bitcoin-cli -regtest generatetoaddress 1 $ADDRESS > /dev/null
+echo $ADDRESS
