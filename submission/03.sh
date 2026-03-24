57b3653541cc1c2210acb4a2f35e2da04da6d79c
72kb <lanniox@gmail.com>
2026-03-24 21:49:46 +0100

    Fourth Commit

--- submission/03.sh
+++ submission/03.sh
@@ -1,8 +1,9 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
-ADDRESS=$(bitcoin-cli -regtest getnewaddress "" "bech32")
-MINER=$(bitcoin-cli -regtest getnewaddress)
-bitcoin-cli -regtest generatetoaddress 101 "$MINER" > /dev/null
-bitcoin-cli -regtest sendtoaddress "$ADDRESS" 1 0 "" "" false 10 > /dev/null
+bitcoin-cli -regtest loadwallet "default" > /dev/null 2>&1
+ADDRESS=$(bitcoin-cli -regtest getnewaddress "funding_addr" "bech32")
+MINER_ADDR=$(bitcoin-cli -regtest getnewaddress "miner_addr")
+bitcoin-cli -regtest generatetoaddress 101 "$MINER_ADDR" > /dev/null
+bitcoin-cli -regtest sendtoaddress "$ADDRESS" 1 > /dev/null
 echo $ADDRESS
