8b28d3857aac6296c27b6286da61bee6cf1ab9fa
72kb <lanniox@gmail.com>
2026-03-24 21:58:56 +0100

    Fifth commit

--- submission/03.sh
+++ submission/03.sh
@@ -5,5 +5,7 @@ bitcoin-cli -regtest loadwallet "default" > /dev/null 2>&1
 ADDRESS=$(bitcoin-cli -regtest getnewaddress "funding_addr" "bech32")
 MINER_ADDR=$(bitcoin-cli -regtest getnewaddress "miner_addr")
 bitcoin-cli -regtest generatetoaddress 101 "$MINER_ADDR" > /dev/null
+# Set a manual fee rate to avoid fee estimation failure in this regtest environment
+bitcoin-cli -regtest settxfee 0.0001 > /dev/null
 bitcoin-cli -regtest sendtoaddress "$ADDRESS" 1 > /dev/null
 echo $ADDRESS
