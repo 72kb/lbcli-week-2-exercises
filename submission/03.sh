1d3de70c4134761866e8da26cfb990ffc4bce63f
72kb <lanniox@gmail.com>
2026-03-24 21:25:42 +0100

    Third Commit

--- submission/03.sh
+++ submission/03.sh
@@ -4,5 +4,5 @@
 ADDRESS=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 MINER=$(bitcoin-cli -regtest getnewaddress)
 bitcoin-cli -regtest generatetoaddress 101 "$MINER" > /dev/null
-bitcoin-cli -regtest sendtoaddress "$ADDRESS" 1 > /dev/null
+bitcoin-cli -regtest sendtoaddress "$ADDRESS" 1 0 "" "" false 10 > /dev/null
 echo $ADDRESS
