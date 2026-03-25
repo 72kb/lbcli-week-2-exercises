cd50544b93e5093f032425f63c023bfebdbf2707
72kb <lanniox@gmail.com>
2026-03-25 07:17:25 +0100

    Sixth COmmit

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,6 @@
 # List the current UTXOs in your wallet.
+# Ensure a UTXO is present in case this runs on a fresh regtest instance
+bitcoin-cli -regtest loadwallet "default" > /dev/null 2>&1 || true
+FAUCET_ADDR=$(bitcoin-cli -regtest getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 "$FAUCET_ADDR" > /dev/null
 bitcoin-cli -regtest listunspent
